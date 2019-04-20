c045d2c6b6d3030af4e1d8d9d830fc8a318bc198
Christian Pinto <pintochristian2@gmail.com>
2019-04-20 21:35:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  list = []
+  name.each do |x|  
+     list << "Hello, my name is #{x}."
+  end
+  return list
+end
+
+def assign_rooms(names)
+  i = 1
+  lists=[]
+  names.each do |x|
+    lists << "Hello, #{x}! You'll be assigned to room #{i}!"
+    i+=1
+  end
+  return lists
+end
+
+def printer(attendees)
+  badge =  batch_badge_creator(attendees)
+  puts badge
+  room =  assign_rooms(attendees)
+end
